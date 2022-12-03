1f783b9376329186e7ea3c751ec591644370c810
freelanceocero <199ocero@gmail.com>
2022-12-03 18:34:25 +0800

    👨‍🔧 minor updates

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="bg-dune w-12 px-10 py-5 items-center flex flex-col justify-between text-2xl text-light-gray"
+    class="bg-dune w-12 px-10 py-5 pt-10 items-center flex flex-col justify-between text-2xl text-light-gray"
   >
     <div class="flex flex-col space-y-12 items-center">
       <i class="fa-regular fa-folder text-white"></i>
