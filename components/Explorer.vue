9234e899956c9887db4aefc5375af57a4bab8104
freelanceocero <199ocero@gmail.com>
2022-12-04 07:47:58 +0800

    👨‍🔧 add class to active route

--- components/Explorer.vue
+++ components/Explorer.vue
@@ -1,43 +1,52 @@
 <template>
   <div
-    class="bg-baltic-sea space-y-12 px-10 py-5 items-center text-2xl text-light-gray"
+    class="items-center px-10 py-5 space-y-12 text-2xl bg-baltic-sea text-light-gray"
   >
-    <div class="flex justify-between space-x-10 items-center">
-      <p class="text-sm text-light-gray uppercase">Explorer</p>
+    <div class="flex items-center justify-between space-x-10">
+      <p class="text-sm uppercase text-light-gray">Explorer</p>
       <div class="flex space-x-1">
-        <div class="bg-light-gray w-1 h-1 rounded-full"></div>
-        <div class="bg-light-gray w-1 h-1 rounded-full"></div>
-        <div class="bg-light-gray w-1 h-1 rounded-full"></div>
+        <div class="w-1 h-1 rounded-full bg-light-gray"></div>
+        <div class="w-1 h-1 rounded-full bg-light-gray"></div>
+        <div class="w-1 h-1 rounded-full bg-light-gray"></div>
       </div>
     </div>
     <div class="space-y-5 text-sm">
-      <div class="flex space-x-2 items-center text-bae">
-        <i class="fa-solid fa-chevron-down text-sm"></i>
-        <p class="uppercase text-base">My Project</p>
+      <div class="flex items-center space-x-2 text-bae">
+        <i class="text-sm fa-solid fa-chevron-down"></i>
+        <p class="text-base uppercase">My Project</p>
       </div>
-      <div class="flex space-x-2 items-center ml-5">
+      <div class="flex items-center ml-5 space-x-2">
         <i class="fa-solid fa-chevron-right"></i>
-        <i class="fa-regular fa-folder-open text-orange-300"></i>
-        <a href="#">home</a>
+        <i class="text-orange-300 fa-regular fa-folder-open"></i>
+        <nuxt-link
+          to="/"
+          class="transition ease-in-out hover:text-orange-300"
+          :class="{
+            'underline decoration-orange-300 text-orange-300':
+              $route.name == 'index',
+          }"
+        >
+          home</nuxt-link
+        >
       </div>
-      <div class="flex space-x-2 items-center ml-5">
+      <div class="flex items-center ml-5 space-x-2">
         <i class="fa-solid fa-chevron-right"></i>
-        <i class="fa-solid fa-folder text-orange-300"></i>
+        <i class="text-orange-300 fa-solid fa-folder"></i>
         <a href="#">about</a>
       </div>
-      <div class="flex space-x-2 items-center ml-5">
+      <div class="flex items-center ml-5 space-x-2">
         <i class="fa-solid fa-chevron-right"></i>
-        <i class="fa-solid fa-folder text-orange-300"></i>
+        <i class="text-orange-300 fa-solid fa-folder"></i>
         <a href="#">projects</a>
       </div>
-      <div class="flex space-x-2 items-center ml-5">
+      <div class="flex items-center ml-5 space-x-2">
         <i class="fa-solid fa-chevron-right"></i>
-        <i class="fa-solid fa-folder text-orange-300"></i>
+        <i class="text-orange-300 fa-solid fa-folder"></i>
         <a href="#">experience</a>
       </div>
-      <div class="flex space-x-2 items-center ml-5">
+      <div class="flex items-center ml-5 space-x-2">
         <i class="fa-solid fa-chevron-right"></i>
-        <i class="fa-solid fa-folder text-orange-300"></i>
+        <i class="text-orange-300 fa-solid fa-folder"></i>
         <a href="#">contact</a>
       </div>
     </div>
