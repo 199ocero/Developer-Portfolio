6badf643c659bf8a8b464d0f77ed93dc290190f6
freelanceocero <199ocero@gmail.com>
2022-12-03 17:03:25 +0800

    ⚡️ add navigation component

---
+++ components/Navigation.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav class="bg-iridium py-2 flex flex-row justify-between px-10">
+    <div class="flex flex-row space-x-3 items-center">
+      <div class="w-3 h-3 rounded-full bg-mac-red"></div>
+      <div class="w-3 h-3 rounded-full bg-mac-yellow"></div>
+      <div class="w-3 h-3 rounded-full bg-mac-green"></div>
+    </div>
+    <p class="text-white text-lg">Developer Portfolio</p>
+    <div class="flex flex-row space-x-3 items-center">
+      <div class="w-3 h-3 rounded-full bg-mac-red"></div>
+      <div class="w-3 h-3 rounded-full bg-mac-yellow"></div>
+      <div class="w-3 h-3 rounded-full bg-mac-green"></div>
+    </div>
+  </nav>
+</template>
