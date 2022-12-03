b936e4080b3e034dd993c3c8567916b521df893a
freelanceocero <199ocero@gmail.com>
2022-12-03 17:18:47 +0800

    👨‍🔧 update navigation

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -5,12 +5,35 @@
       <div class="w-3 h-3 rounded-full bg-mac-yellow"></div>
       <div class="w-3 h-3 rounded-full bg-mac-green"></div>
     </div>
+
+    <!-- Change it the title of your website -->
     <p class="text-white text-lg">Developer Portfolio</p>
-    <div class="flex flex-row space-x-3 items-center">
-      <div class="w-3 h-3 rounded-full bg-mac-red"></div>
-      <div class="w-3 h-3 rounded-full bg-mac-yellow"></div>
-      <div class="w-3 h-3 rounded-full bg-mac-green"></div>
-      <i class="fa-solid fa-house"></i>
+
+    <div class="flex flex-row space-x-5 text-white">
+      <!-- You can add your social accounts here -->
+      <a
+        href="https://gitlab.com/ocero-freelance-projects"
+        class="transition ease-in-out hover:text-yellow-300"
+        ><i class="fa-brands fa-github"></i
+      ></a>
+      <a
+        href="https://www.behance.net/freelance-ocero"
+        class="transition ease-in-out hover:text-yellow-300"
+      >
+        <i class="fa-brands fa-behance"></i
+      ></a>
+      <a
+        href="https://www.linkedin.com/in/jay-are-ocero-891b63208/"
+        class="transition ease-in-out hover:text-yellow-300"
+      >
+        <i class="fa-brands fa-linkedin-in"></i
+      ></a>
+      <a
+        href="https://www.facebook.com/jaocero/"
+        class="transition ease-in-out hover:text-yellow-300"
+      >
+        <i class="fa-brands fa-facebook-f"></i
+      ></a>
     </div>
   </nav>
 </template>
