d0a671294a3258f04af903073d03b8b61a0f5527
freelanceocero <freelanceocero@gmail.com>
2022-12-04 11:04:08 +0800

    ✨ add contact page

--- components/Explorer.vue
+++ components/Explorer.vue
@@ -85,8 +85,23 @@
       </div>
       <div class="flex items-center ml-5 space-x-2">
         <i class="fa-solid fa-chevron-right"></i>
-        <i class="text-orange-300 fa-solid fa-folder"></i>
-        <a href="#">contact</a>
+        <i
+          class="text-orange-300 fa-regular fa-folder"
+          :class="{
+            'text-orange-300 fa-regular fa-folder-open':
+              $route.name == 'contact',
+          }"
+        ></i>
+        <nuxt-link
+          to="/contact"
+          class="transition ease-in-out hover:text-orange-300"
+          :class="{
+            'underline decoration-orange-300 text-orange-300':
+              $route.name == 'contact',
+          }"
+        >
+          contact</nuxt-link
+        >
       </div>
     </div>
   </div>
