7e306c5c6105a3b507c9176e0811c6d1f2848e4d
freelanceocero <freelanceocero@gmail.com>
2022-12-04 12:49:40 +0800

    👨‍🔧 update social sidebar

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -2,14 +2,32 @@
   <div
     class="flex-col items-center justify-between hidden w-12 px-10 py-5 text-2xl lg:flex bg-dune text-light-gray"
   >
+    <i class="fa-solid fa-fire-flame-curved text-mac-yellow"></i>
     <div class="flex flex-col items-center space-y-12">
-      <i class="fa-solid fa-fire-flame-curved text-mac-yellow"></i>
-      <i class="text-white fa-regular fa-folder"></i>
-      <i class="fa-solid fa-magnifying-glass"></i>
-      <i class="fa-solid fa-code-branch"></i>
-      <i class="fa-solid fa-bug"></i>
-      <i class="fa-solid fa-database"></i>
+      <a
+        href="https://github.com/199ocero"
+        class="transition ease-in-out hover:text-mac-yellow"
+        ><i class="fa-brands fa-github"></i
+      ></a>
+      <a
+        href="https://www.behance.net/freelance-ocero"
+        class="transition ease-in-out hover:text-mac-yellow"
+      >
+        <i class="fa-brands fa-behance"></i
+      ></a>
+      <a
+        href="https://www.linkedin.com/in/jay-are-ocero-891b63208/"
+        class="transition ease-in-out hover:text-mac-yellow"
+      >
+        <i class="fa-brands fa-linkedin-in"></i
+      ></a>
+      <a
+        href="https://www.facebook.com/jaocero/"
+        class="transition ease-in-out hover:text-mac-yellow"
+      >
+        <i class="fa-brands fa-facebook-f"></i
+      ></a>
+      <div class="w-1 h-20 rounded-full bg-light-gray opacity-20"></div>
     </div>
-    <i class="fa-solid fa-circle-user"></i>
   </div>
 </template>
