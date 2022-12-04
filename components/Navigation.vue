4aad56a5ad918e6f34a678191e8a6eb1377add3b
freelanceocero <freelanceocero@gmail.com>
2022-12-04 10:37:57 +0800

    ✨ add social component

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -42,31 +42,9 @@
           }"
         ></span>
       </a>
-      <!-- You can add your social accounts here -->
-      <div class="flex-row hidden space-x-5 lg:flex text-light-gray">
-        <a
-          href="https://gitlab.com/ocero-freelance-projects"
-          class="transition ease-in-out hover:text-mac-yellow"
-          ><i class="fa-brands fa-github"></i
-        ></a>
-        <a
-          href="https://www.behance.net/freelance-ocero"
-          class="transition ease-in-out hover:text-mac-yellow"
-        >
-          <i class="fa-brands fa-behance"></i
-        ></a>
-        <a
-          href="https://www.linkedin.com/in/jay-are-ocero-891b63208/"
-          class="transition ease-in-out hover:text-mac-yellow"
-        >
-          <i class="fa-brands fa-linkedin-in"></i
-        ></a>
-        <a
-          href="https://www.facebook.com/jaocero/"
-          class="transition ease-in-out hover:text-mac-yellow"
-        >
-          <i class="fa-brands fa-facebook-f"></i
-        ></a>
+      <!-- Social Icons -->
+      <div class="hidden lg:flex">
+        <Social />
       </div>
     </nav>
     <div
---
+++ components/Social.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- You can add your social accounts here -->
+  <div class="flex flex-row space-x-5 text-light-gray">
+    <a
+      href="https://github.com/199ocero"
+      class="transition ease-in-out hover:text-mac-yellow"
+      ><i class="fa-brands fa-github"></i
+    ></a>
+    <a
+      href="https://www.behance.net/freelance-ocero"
+      class="transition ease-in-out hover:text-mac-yellow"
+    >
+      <i class="fa-brands fa-behance"></i
+    ></a>
+    <a
+      href="https://www.linkedin.com/in/jay-are-ocero-891b63208/"
+      class="transition ease-in-out hover:text-mac-yellow"
+    >
+      <i class="fa-brands fa-linkedin-in"></i
+    ></a>
+    <a
+      href="https://www.facebook.com/jaocero/"
+      class="transition ease-in-out hover:text-mac-yellow"
+    >
+      <i class="fa-brands fa-facebook-f"></i
+    ></a>
+  </div>
+</template>
